69f9081e862da29008b56dfb3f3dc73f526de285
Octowl <alsaff1987@gmail.com>
2019-01-07 21:03:08 +0300

    Remove redundant loading from booklist

--- src/BookList.js
+++ src/BookList.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { observer } from "mobx-react";
 
 // Components
-import Loading from "./Loading";
 import SearchBar from "./SearchBar";
 import BookTable from "./BookTable";
 
@@ -18,9 +17,7 @@ class BookList extends Component {
       books = bookStore.filterBooksByColor(bookColor);
     }
 
-    return bookStore.loading ? (
-      <Loading />
-    ) : (
+    return (
       <div>
         <h3>Books</h3>
         <SearchBar store={bookStore} />
